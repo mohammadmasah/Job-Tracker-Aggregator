6df8dd4dc88c169682932a5191e83bc256577fcf
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-07-07 15:07:26 +0200

    Add login api

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,20 @@
 from sqlmodel import Field, SQLModel
+from pydantic import Field as PydanticField
+
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field
-    lastname: str = Field
+    name: str 
+    lastname: str 
     email: str = Field (index=True, unique=True)
     hashed_password: str 
     
+class UserCreate(SQLModel):
+    name: str
+    lastname: str
+    email: str
+    password: str
+
+class UserLogin(SQLModel):
+    email: str
+    password: str
--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -2,18 +2,37 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlmodel import Session, select
 
 from ..database import get_session
-from ..models import User
+from ..models import User, UserCreate, UserLogin
+from app.core.security import hash_password, verify_password
+
 
 router = APIRouter(prefix="/api/user", tags=["user"])
 
-# Create user
-@router.post("")
-def create_user(user: User, session: Session = Depends(get_session)):
-    session.add(user)
+# REGISTER + BCRYP
+@router.post("/register")
+def create_user(user: UserCreate, session: Session = Depends(get_session)):
+    db_user = User (
+        name=user.name,
+        lastname=user.lastname,
+        email=user.email,
+        hashed_password=hash_password(user.password)
+    )
+    session.add(db_user)
     session.commit()
-    session.refresh(user)
-    return user
+    session.refresh(db_user)
+    return {"message": "User created"}
 
+# LOGIN
+@router.post("/login")
+def login(credentials: UserLogin, session: Session = Depends(get_session)):
+    user = session.exec(select(User).where(User.email == credentials.email)).first()
+    if not user: 
+        raise HTTPException(status_code=401, detail="Invalid identification")
+    if not verify_password(credentials.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid identification")
+    return {"message": "Connected " + user.name}
+    
+    
 # GET
 # All
 @router.get("")
